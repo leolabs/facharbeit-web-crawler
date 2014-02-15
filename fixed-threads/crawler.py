1974df19f7be6c30beb1ed426ab186ce8d74c307
Leo Bernard <admin@leolabs.org>
2014-02-15 12:25:57 +0100

    Code komplett kommentiert, kleine Performancefehler behoben

--- fixed-threads/crawler.py
+++ fixed-threads/crawler.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-__author__ = 'leobernard'
+__author__ = 'Leo Bernard'
 
 """
 Die Crawler-Funktionen des Projektes.
@@ -17,78 +17,89 @@ import time  # Bibltiothek zur Zeitmessung laden
 from bs4 import BeautifulSoup, Comment  # Bibliothek zum Auslesen von Elementen aus dem HTML Code laden
 
 
-class Crawler(threading.Thread):
+class Worker(threading.Thread):
     def __init__(self, worker_id, client, queue, options):
         threading.Thread.__init__(self)
         self.worker_id = worker_id
         self.client = client
         self.queue = queue
         self.options = options
+        self.is_killed = False
+
+    def kill_worker(self):
+        """Sagt dem Worker, dass er sich nach dem aktuellen Auftrag beenden soll."""
+        print "\t[%03d] Worker wird so bald wie möglich gestoppt." % self.worker_id
+        self.is_killed = True
 
     def filter_readabletext(self, element):
         """Prüft, ob das gegebene HTML-Element ein gültiger Text ist."""
         if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
-            return False
+            return False  # Tags, die nicht mit dem Dokument zu tun haben, ausschließen
         elif isinstance(element, Comment):
-            return False
+            return False  # HTML-Kommentare ausschließen
         elif unicode(element) in [u"\n", u' ']:
-            return False
+            return False  # Leere Tags ausschließen
         elif len(element) < 3:
-            return False
+            return False  # Tags mit weniger als drei Zeichen ausschließen
         elif not re.match(ur'([A-Za-z])+', unicode(element)):
-            return False
+            return False  # Tags, die keine Buchstaben enthalten, ausschließen
         elif not u' ' in unicode(element).strip():
-            return False
+            return False  # Tags, die keine Leerzeichen enthalten, ausschließen
         else:
-            return True
+            return True  # Ansonsten kann der Block mit in die Liste der Links aufgenommen werden
 
     def run(self):
         """
         Lädt eine gegebene Seite, extrahiert die benötigten Metadaten und fügt diese in die Datenbank ein.
         Dazu werden alle auf der geladenen Seite vorhandenen Links in die Warteschlange eingefügt.
         """
-        queue = self.client['crawler-database']['queue']
-        crawled = self.client['crawler-database']['crawled']
+        queue = self.client['crawler-database']['queue']  # Queue aus der Datenbank auswählen
+        crawled = self.client['crawler-database']['crawled']  # Liste der gecrawlten Websites aus Datenbank auswählen
 
-        if int(self.options.verbosity) >= 1:
+        if int(self.options.verbosity) >= 1:  # Benutzer bescheidgeben, dass der Worker nun läuft
             print "\t[%03d] Worker wurde gestartet, wartet auf Aufträge." % self.worker_id
 
-        while True:
-            if not self.queue.empty():
-                current_job = self.queue.get()
+        while True:  # Unendlichschleife starten, die die Warteschlange auf Einträge überprüft und diese ggf. abarbeitet
+            if self.is_killed:  # Wenn der Worker als zu beenden markiert ist, Schleife verlassen
+                print "\t[%03d] Worker wurde gestoppt." % self.worker_id
+                break
+
+            if not self.queue.empty():  # Wenn es Aufträge in der Warteschlange gibt, diese verarbeiten
+                current_job = self.queue.get()  # Auftrag aus der Warteschlange entnehmen
                 try:
                     if int(self.options.verbosity) >= 1:
                         print "\t[%03d] [" % self.worker_id + str(current_job[u'depth']) + "] [GET] " + \
                               current_job[u'url'] + " wird aufgerufen..."
 
-                    time_start = int(round(time.time() * 1000))
+                    time_start = int(round(time.time() * 1000))  # Zeit vor dem Aufruf speichern
 
+                    # Website-Aufruf initialisieren
                     req = urllib2.Request(
                         current_job[u'url'],
                         headers={'User-Agent': "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 "
                                                "(KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36"}
                     )
 
-                    con = urllib2.urlopen(req)
-                    source = con.read()
-                    time_response = int(round(time.time() * 1000))
-                    soup = BeautifulSoup(source)
-                except Exception, e:
+                    con = urllib2.urlopen(req)  # Website aufrufen
+                    source = con.read()  # Inhalt der Website auslesen
+                    time_response = int(round(time.time() * 1000))  # Zeit nach dem Aufruf speichern
+                    soup = BeautifulSoup(source)  # Empfangenen HTML-Code parsen
+                except Exception, e:  # Wenn ein Fahler aufgetreten ist, diesen ausgeben
                     print "\t[%03d] [" % self.worker_id + str(current_job[u'depth']) + "] [!!!] " + \
                           current_job[u'url'] + " ist fehlerhaft:", e
                     pass
-                else:
+                else:  # Wenn bis jetzt alle Abfragen fehlerfrei ausgeführt wurden
                     if int(self.options.verbosity) >= 2:
                         print "\t[%03d] [" % self.worker_id + str(current_job[u'depth']) + "] [" + \
                               str(con.getcode()) + "] " + current_job[u'url'] + " hat geantwortet..."
 
                     # Scraping
-                    soup_links = soup.findAll('a')
-                    soup_title = soup.find('title')
-                    soup_author = soup.find('meta', {'name': 'author'})
-                    soup_description = soup.find('meta', {'name': 'description'})
-                    soup_keywords = soup.find('meta', {'name': 'keywords'})
-                    soup_texts = soup.findAll(text=True)
+                    soup_links = soup.findAll('a')  # Alle Links finden
+                    soup_title = soup.find('title')  # Titel der Seite finden
+                    soup_author = soup.find('meta', {'name': 'author'})  # Author der Seite finden
+                    soup_description = soup.find('meta', {'name': 'description'})  # Beschreibung der Seite finden
+                    soup_keywords = soup.find('meta', {'name': 'keywords'})  # Keywords der Seite finden
+                    soup_texts = soup.findAll(text=True)  # Alle Text-Blöcke finden
 
                     # Einzufügendes Objekt erstellen
                     urlobject = {
@@ -98,7 +109,7 @@ class Crawler(threading.Thread):
                         "description": soup_description['content'] if soup_description is not None else "",
                         "keywords": soup_keywords['content'] if soup_keywords is not None else "",
                         "linkcount": len(soup_links),
-                        "textblocks": filter(self.filter_readabletext, soup_texts),
+                        "textblocks": "\n;\n".join(filter(self.filter_readabletext, soup_texts)),
                         "depth": current_job[u'depth'],
                         "parent": current_job[u'parent'],
                         "latency": time_response - time_start,
@@ -117,14 +128,19 @@ class Crawler(threading.Thread):
                         crawled.find_and_modify({'_id': current_job[u'parent']}, {'$push': {'children': doc_id}})
 
                     if int(current_job[u'depth']) < int(self.options.depth):  # Maximale Tiefe nicht überschreiten
-                        linklist = []
-                        for item in soup.findAll('a'):
+                        linklist = []  # Liste der bereits gefundenen Links anlegen
+                        for item in soup.findAll('a'):  # Alle Links finden
                             try:
+                                # Relative URLs zu absoluten URLs parsen
                                 link = urlparse.urljoin(current_job[u'url'], item['href'])
+
+                                # Wenn der Link nicht bereits in der Liste ist und auf eine andere Website zeigt
                                 if not link in linklist and not link.startswith("#") \
                                         and not link.startswith("mailto") and not link.startswith("javascript")\
                                         and not re.match(".*\.(jpg|jpeg|png|gif|pdf)", link):
-                                    linklist.append(link)
+                                    linklist.append(link)  # Link zu der Liste der bereits gefundenen Links hinzufügen
+
+                                    # Link in die Queue der Datenbank einfügen
                                     queue.insert({
                                         'url': link.split("#", 1)[0],
                                         'depth': current_job[u'depth'] + 1,
--- fixed-threads/readtest.py
+++ fixed-threads/readtest.py
@@ -1,4 +1,13 @@
 # coding=utf-8
+__author__ = "Leo Bernard"
+
+"""
+Der Queue-Manager des Projektes, zudem das Main-Script
+
+Im Rahmen der Facharbeit "Datenbanken - Big Data" von Leo Bernard
+
+"""
+
 import sys  # Systembezogene Funktionen laden
 import Queue  # Datenstruktur Warteschlange laden
 import crawler  # Crawler-Funktionen laden (siehe crawler.py)
@@ -27,43 +36,53 @@ parser.add_option("-v", "--verbosity", dest="verbosity", default="2",
 (options, args) = parser.parse_args()  # Gegebene Parameter laden.
 
 # ============================== Initialisierung starten ============================== #
-client = MongoClient(max_pool_size=500)
-queue = client['crawler-database']['queue']
-crawled = client['crawler-database']['crawled']
+client = MongoClient(max_pool_size=500)  # Verbindung zu unserer Datenbank (MongoDB) herstellen
+queue = client['crawler-database']['queue']  # Warteschlange aus der Datenbank auswählen
+crawled = client['crawler-database']['crawled']  # Liste der gecrawlten Websites aus der Datenbank auswählen
 
-if options.website is not False:
+if options.website is not False:  # Wenn eine Start-Website als Parameter angegeben wurde.
     queue.drop()
     crawled.drop()
     queue.insert({'url': options.website, 'parent': 0, 'depth': 0})
+    print "Der Crawler wird die bestehenden Daten löschen und mithilfe von", options.threads, \
+          "Workern alle Websites bis zur Tiefe", options.depth, "ausgehend von der Adresse " + \
+          options.website + " per " + ("Breitensuche" if options.mode == "b" else "Tiefensuche") + " durchsuchen."
+    print "====================================================================="
+else:
     print "Der Crawler wird mithilfe von", str(options.threads), "Workern alle Websites bis zur Tiefe", \
-          options.depth, "ausgehend von der Adresse " + options.website + " per " + \
-          ("Breitensuche" if options.mode == "b" else "Tiefensuche") + " durchsuchen."
+          options.depth, "per" + ("Breitensuche" if options.mode == "b" else "Tiefensuche") + \
+          " durchsuchen, die sich in der Warteschlange der Datenbank befinden."
     print "====================================================================="
 
-counter = 1
-job_queue = Queue.Queue(0)
-workers = []
+counter = 1  # Zählvariable einführen, welche die Anzahl der bereits abgefragten Websites speichert
+job_queue = Queue.Queue(0)  # Lokale Warteschlange initialisieren, welche später die Jobs für alle Worker enthalten wird
+workers = []  # Liste der Worker initialisieren
 
-for i in range(1, int(options.threads) + 1):
-    thread = crawler.Crawler(i, client, job_queue, options)
-    thread.setDaemon(True)
-    thread.start()
-    workers.append(thread)
+for i in range(1, int(options.threads) + 1):  # Die gegebene Anzahl von Workern initialisieren
+    thread = crawler.Worker(i, client, job_queue, options)  # Worker erstellen
+    thread.start()  # Worker starten
+    workers.append(thread)  # Worker der Liste der Worker hinzufügen
 
-try:
-    while True:
-        document = queue.find_one()
+try:  # Fehler abfangen
+    while True:  # Endlosschleife starten, welche die Queue-Items der Datenbank abfragt und in die lokale Queue einfügt
+        document = queue.find_and_modify(remove=True)  # Erstes Item der Queue aus der Datenbank abrufen und entfernen
 
         if document is not None:
             if int(document[u'depth']) <= int(options.depth):
-                queue.remove({'_id': document[u'_id']})
                 job_queue.put(document)
                 counter += 1
-            else:
-                queue.remove({'_id': document[u'_id']})
 
-# Wenn das Programm beendet wird, alle Threads beenden
-except KeyboardInterrupt:
+
+except KeyboardInterrupt:  # Wenn das Programm beendet wird, alle Threads beenden
+    print "========================================================================"
+    print "Worker werden beendet..."
+
+    for worker in workers:  # Alle laufenden Worker als zu beenden markieren
+        worker.kill_worker()
+
+    for worker in workers:  # Warten, bis alle Worker beendet wurden
+        worker.join()
+
     print "========================================================================"
     print "Crawling wurde beendet. Insgesamt wurden", counter, "Websites abgefragt."
     sys.exit(0)
